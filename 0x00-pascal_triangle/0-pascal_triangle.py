0a7304bbb4a5e8d945f7fac1a9db894fe01914bd
Yoseph <ymymym4634@gmail.com>
2023-07-02 22:13:09 +0300

    pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,21 @@
-# pascal's triangle
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+def pascal_triangle(n):
+    triangle = []
+
+    for i in range(n):
+        row = []
+        
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                # the first and last elements in each row are always 1
+                row.append(1)
+            else:
+                # calculate the value based on the sum of the two numbers above
+                value = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                row.append(value)
+        
+        triangle.append(row)
+    
+    return triangle
