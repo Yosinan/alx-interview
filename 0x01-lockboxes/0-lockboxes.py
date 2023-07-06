fdb8294e83d82df7426ba5ad07b05c06a84a1513
Yoseph <ymymym4634@gmail.com>
2023-07-06 19:36:03 +0300

    alx-interview

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
-    n = len(boxes)  # no of boxes
-    visited = set()  # set to store visited boxes
-    queue = [0]  # Queue to store boxes for checking
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+        boxes (list): A list of lists representing the boxes and their corresponding keys.
+                      Each box is numbered sequentially from 0 to n - 1.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+
+    """
+
+    n = len(boxes)  # Number of boxes
+    visited = set()  # Set to store visited boxes
+    queue = [0]  # Queue to store boxes for exploration
 
     while queue:
         box = queue.pop(0)
