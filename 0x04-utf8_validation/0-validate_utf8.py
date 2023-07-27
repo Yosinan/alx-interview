06900395f87de46872a9280ff431636176efcfcb
Yoseph <ymymym4634@gmail.com>
2023-07-27 21:04:04 +0300

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -18,18 +18,18 @@ def validUTF8(data):
     for num in data:
         byte = num & 0xff
         if no_bytes == 0:
-            mask = 1 << 7
-            while mask & byte:
+            bit_mask = 1 << 7
+            while bit_mask & byte:
                 no_bytes += 1
-                mask >>= 1
+                bit_mask >>= 1
             if no_bytes == 0:
                 continue
             if no_bytes == 1 or no_bytes > 4:
                 return False
         else:
-            bit_mask = 1 << 7
+            bit_mask1 = 1 << 7
             bit_mask2 = 1 << 6
-            if not (byte & bit_mask and not (byte & bit_mask2)):
+            if not (byte & bit_mask1 and not (byte & bit_mask2)):
                 return False
         no_bytes -= 1
     return no_bytes == 0
