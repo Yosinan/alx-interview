b3743e0832e798686bc195eac17602533d830a14
Yoseph <ymymym4634@gmail.com>
2023-08-04 01:03:39 +0300

    N Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -8,22 +8,23 @@ def is_safe(board, row, col, N):
     for i in range(row):
         if board[i] == col:
             return False
-    
         # Check diagonals
         if abs(i - row) == abs(board[i] - col):
             return False
-    
     return True
 
+
 def solve_nqueens(N):
+    """Solves the N queens problem"""
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
     solutions = []
     board = [-1] * N
     
     def solve(row):
+
+
         if row == N:
             solutions.append([(i, board[i]) for i in range(N)])
             return
@@ -33,15 +34,15 @@ def solve_nqueens(N):
                 board[row] = col
                 solve(row + 1)
                 board[row] = -1
-    
     solve(0)
     return solutions
 
 def main():
+
+
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
     try:
         N = int(sys.argv[1])
     except ValueError:
@@ -49,7 +50,7 @@ def main():
         sys.exit(1)
     
     solutions = solve_nqueens(N)
-    
+
     for solution in solutions:
         print(solution)
 
