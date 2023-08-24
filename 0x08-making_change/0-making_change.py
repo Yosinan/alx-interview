90b17118d0dd778d9a70edbb86dcb1cc7268c189
Yosinan <ymymym4634@gmail.com>
2023-08-24 21:28:54 +0000

    Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-''' make change '''
+''' making change '''
 
 
 def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    ''' Initialize a list to store the minimum number
-    of coins needed for each amount from 0 to total'''
-    dp = [sys.maxsize] * (total + 1)
+    dp = [float('inf')] * (total + 1)
     dp[0] = 0
-    m = len(coins)
-    for i in range(1, total + 1):
-        for j in range(m):
-            if coins[j] <= i:
-                sub_res = dp[i - coins[j]]
-                if sub_res != sys.maxsize and sub_res + 1 < dp[i]:
-                    dp[i] = sub_res + 1
 
-    return -1 if dp[total] == sys.maxsize else dp[total]
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
